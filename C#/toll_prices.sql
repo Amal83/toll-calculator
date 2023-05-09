3a1afe8777fbc57fa06b0f93f1e61a76022d4e40
aq-afry <amal.qasim@afry.com>
2023-05-09 15:26:23 +0200

    all about docker

---
+++ C#/toll_prices.sql
@@ -0,0 +1,18 @@
+CREATE TABLE toll_prices (
+  ID INT PRIMARY KEY AUTO_INCREMENT,
+  start_time TIME,
+  end_time TIME,
+  toll_price NUMERIC(5,2)
+);
+
+INSERT INTO toll_prices (start_time, end_time, toll_price)
+VALUES
+  ('06:00:00', '06:29:59', 8),
+  ('06:30:00', '06:59:59', 13),
+  ('07:00:00', '07:59:59', 18),
+  ('08:00:00', '08:29:59', 13),
+  ('08:30:00', '14:59:59', 8),
+  ('15:00:00', '15:29:59', 13),
+  ('15:30:00', '16:59:59', 18),
+  ('17:00:00', '17:59:59', 13),
+  ('18:00:00', '23:59:59', 8);
